35efcb24c68a5d984709b09460b43400cef6d89a
Nicolas Strella <strella.nicolas@gmail.com>
2017-09-18 15:36:19 -0300

    Creadas las clases Jugador y Tablero

--- Jugador.java
+++ Jugador.java
@@ -1,4 +1,24 @@
+import java.util.Vector;
 
 public class Jugador {
+	private String name;
+	private Vector<Carta> mazo;
 
+	public Jugador(String name){
+		this.name = name;
+		mazo = new Vector<Carta>();
+	}
+
+	//esto nomas para poner un mensaje onda "gano Pancho" xD
+	public String getName(){
+		return name;
+	}
+
+	/*
+		Ademas, jugador cuenta con los metodos de Vector:
+
+		add() //para añadir cartas al mazo cuando se reparten las cartas entre jugadores o cuando gana la ronda y gana la carta del oponente
+		removeElementAt() //para eliminar la carta en caso de perder la ronda
+		isEmpty() //si se queda sin cartas, termina la partida y pierde
+	*/
 }
--- Tablero.java
+++ Tablero.java
@@ -1,4 +1,46 @@
 
 public class Tablero {
 
+	/*
+	dejo esto comentado por que no se si queres que Tablero contenga a los jugadores y al mazo o los queres en el main como estan en Juego
+
+	private Jugador player1;
+	private Jugador player2;
+	private Mazo mazo;
+
+	public Tablero(){
+		//mazo por defecto
+	}
+
+	public Tablero(String jugador1, String jugador2){
+		//utilizar un mazo por defecto
+		Jugador player1 = new Jugador(jugador1);
+		Jugador player2 = new Jugador(jugador2);
+	}
+
+	public void setJugadores(String nameP1, String nameP2){
+		player1 = new Jugador(nameP1);
+		player2 = new Jugador(nameP2);
+	}
+
+	public void jugar(){
+
+	//deberiamos crear una clase q sea JUGAR para delegarle esta responsabilidad a dicha clase..
+	//de esta forma si queremos añadir mas estilos de juego ademas de normal y por ronda, podemos hacerlo sin tener q añadir mas codigo aca
+	//nuevamente, te lo dejo comentado para q me des tu opinion
+
+	//aca sino seria un if o case... si es un modo de juego, se juega de esa forma y sino, se juega de la otra
+	//en lo unico q cambiara sera que uno sera un for con un index pasado por parametro y el otro sera un while hasta q uno de los dos jugadores se quede sin cartas
+
+	}
+
+	private void ganador(){
+
+		//se comparan las cartas y se decide quien gano esa ronda.. el que gano se lleva las cartas de la ronda, el q perdio pierde la carta de su mazo, si es empate, se juega de nuevo
+
+	}
+
+	
+
+	*/
 }
